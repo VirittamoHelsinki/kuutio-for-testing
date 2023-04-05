95f535dccd29922c1b8b4bd4b676d66c33e63ea9
mikkom79 <mikkomanninen79@gmail.com>
2023-04-05 21:43:39 +0300

    out comments removed from firebase config file

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-//import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6LYpk_DFZwixdmW2AAJ-AErsJ7gnRyCk",
@@ -13,8 +11,6 @@ const firebaseConfig = {
   appId: "1:676652676649:web:4c4a7483ff4850274c8e38",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-//export const storage = getStorage(app);
